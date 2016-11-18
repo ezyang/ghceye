0a7ef896997694d950e086b62aaf78fda820840f
Michael Snoyman <michael@snoyman.com>
2016-11-18 08:10:09 +0200

    Put timeout on waitForProcess

--- Proc.hs
+++ Proc.hs
@@ -7,8 +7,10 @@ import System.Timeout
 main = do
     hSetBuffering stderr NoBuffering
     hSetBuffering stdout NoBuffering
+
     putStrLn "creating pipe"
     (readOut, writeOut) <- createPipe
+
     putStrLn "calling createProcess"
     (Just writeIn,_,_,proch) <- createProcess (proc "python" ["-i"]) {
             std_in = CreatePipe,
@@ -16,22 +18,33 @@ main = do
             std_err = UseHandle writeOut,
             close_fds = True
         }
+
     putStrLn "closing writeOut"
     hClose writeOut
+
     putStrLn "sending print(42)"
     S.hPutStrLn writeIn "print(42)"
+
     putStrLn "sending quit()"
     S.hPutStrLn writeIn "quit()"
+
     putStrLn "closing writeInt"
     hClose writeIn
+
+    let timeout10s = timeout (1000 * 1000 * 10) -- 10 seconds
+
     putStrLn "waiting for process"
-    exit <- waitForProcess proch
-    putStrLn "calling timeout"
-    mres <- timeout (1000 * 1000 * 10) $ do -- 10 seconds
-        putStrLn "calling hGetContents"
-        S.hGetContents readOut
+    mexit <- timeout10s $ waitForProcess proch
+    exit <- case mexit of
+        Nothing -> error "Waiting for process timed out"
+        Just exit -> return exit
+
+    putStrLn "calling hGetContents"
+    mres <- timeout10s $ S.hGetContents readOut
+
     putStrLn "closing readOut"
     hClose readOut
+
     case mres of
         Nothing -> error "Did not complete"
         Just bs -> do
