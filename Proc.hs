0b7f0e675016336757b1b2f2a553001552f94b72
Edward Z. Yang <ezyang@cs.stanford.edu>
2016-11-17 16:47:43 -0800

    OK do something
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- Proc.hs
+++ Proc.hs
@@ -1,1 +1,16 @@
-main = return ()
+import System.Process
+import System.IO
+import System.Exit
+main = do
+    (readEnd, writeEnd) <- createPipe
+    (_,_,_,proch) <- createProcess (proc "ghci" []) {
+            std_in = UseHandle readEnd,
+            close_fds = True,
+            create_group = True
+        }
+    hPutStrLn writeEnd ":set prompt \"\""
+    hPutStrLn writeEnd "print 42"
+    hPutStrLn writeEnd ":quit"
+    hClose writeEnd
+    exit <- waitForProcess proch
+    exitWith exit
