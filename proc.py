b9e762701779584d21c067ef1c33b3a2a1e9c26c
Edward Z. Yang <ezyang@cs.stanford.edu>
2016-11-17 17:19:29 -0800

    no close fds
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- proc.py
+++ proc.py
@@ -3,7 +3,7 @@ import os
 
 (rin, win) = os.pipe()
 (rout, wout) = os.pipe()
-p = subprocess.Popen(["python", "-i"], stdin=rin, stdout=wout, stderr=wout, close_fds=True)
+p = subprocess.Popen(["python", "-i"], stdin=rin, stdout=wout, stderr=wout)
 os.write(win, "print(2)")
 os.write(win, os.linesep)
 os.write(win, "quit()")
