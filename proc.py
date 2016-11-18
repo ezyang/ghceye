fd201076bb2432f73db7878612eb1ca72412a290
Edward Z. Yang <ezyang@cs.stanford.edu>
2016-11-17 17:19:57 -0800

    Revert "no close fds"
    This reverts commit b9e762701779584d21c067ef1c33b3a2a1e9c26c.

--- proc.py
+++ proc.py
@@ -3,7 +3,7 @@ import os
 
 (rin, win) = os.pipe()
 (rout, wout) = os.pipe()
-p = subprocess.Popen(["python", "-i"], stdin=rin, stdout=wout, stderr=wout)
+p = subprocess.Popen(["python", "-i"], stdin=rin, stdout=wout, stderr=wout, close_fds=True)
 os.write(win, "print(2)")
 os.write(win, os.linesep)
 os.write(win, "quit()")
