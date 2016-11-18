a818260cc98f9ab88ad003eba8261ab05ce4eb20
Michael Snoyman <michael@snoyman.com>
2016-11-18 08:04:29 +0200

    More debugging output

--- Proc.hs
+++ Proc.hs
@@ -7,22 +7,35 @@ import System.Timeout
 main = do
     hSetBuffering stderr NoBuffering
     hSetBuffering stdout NoBuffering
+    putStrLn "creating pipe"
     (readOut, writeOut) <- createPipe
+    putStrLn "calling createProcess"
     (Just writeIn,_,_,proch) <- createProcess (proc "python" ["-i"]) {
             std_in = CreatePipe,
             std_out = UseHandle writeOut,
             std_err = UseHandle writeOut,
             close_fds = True
         }
+    putStrLn "closing writeOut"
     hClose writeOut
+    putStrLn "sending print(42)"
     S.hPutStrLn writeIn "print(42)"
+    putStrLn "sending quit()"
     S.hPutStrLn writeIn "quit()"
+    putStrLn "closing writeInt"
     hClose writeIn
+    putStrLn "waiting for process"
     exit <- waitForProcess proch
-    mres <- timeout
-        (1000 * 1000 * 10) -- 10 seconds
-        (S.hPutStrLn stderr =<< S.hGetContents readOut)
+    putStrLn "calling timeout"
+    mres <- timeout (1000 * 1000 * 10) $ do -- 10 seconds
+        putStrLn "calling hGetContents"
+        S.hGetContents readOut
+    putStrLn "closing readOut"
     hClose readOut
     case mres of
         Nothing -> error "Did not complete"
-        Just () -> exitWith exit
+        Just bs -> do
+            putStrLn "sending output to stderr"
+            S.hPutStrLn stderr bs
+            putStrLn "exiting"
+            exitWith exit
